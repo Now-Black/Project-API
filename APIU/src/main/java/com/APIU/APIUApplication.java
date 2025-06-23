d5405b1a90b1ce09294da7780e3727d3686f6fdf
black <1094855191@qq.com>
2025-06-23 17:23:09 +0800

    commit1

---
+++ APIU/src/main/java/com/APIU/APIUApplication.java
@@ -0,0 +1,21 @@
+package com.APIU;
+
+
+import org.mybatis.spring.annotation.MapperScan;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.scheduling.annotation.EnableAsync;
+import org.springframework.scheduling.annotation.EnableScheduling;
+import org.springframework.transaction.annotation.EnableTransactionManagement;
+
+
+@EnableAsync
+@SpringBootApplication(scanBasePackages = {"com.APIU"})
+@EnableTransactionManagement
+@EnableScheduling
+@MapperScan("com.APIU.mappers")
+public class APIUApplication {
+    public static void main(String[] args) {
+        SpringApplication.run(APIUApplication.class,args);
+    }
+}
